eb9d30b7416de836e3b781bdf996b20c498a8ad4
masahiro.yanai <masahiro.yanai@showroom.co.jp>
2020-10-18 23:26:41 +0900

    memoryAccess

---
+++ concurrency/memoryAccess/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var data int
+	go func() { data++ }()
+	if data == 0 {
+		fmt.Println("the value is 0.")
+	} else {
+		fmt.Println("the value is %v.\n", data)
+	}
+}
