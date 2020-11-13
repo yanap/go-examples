2e0d3a4f23aa72a8454b64c14eae5a554c2dc75d
masahiro.yanai <masahiro.yanai@showroom.co.jp>
2020-11-13 11:21:33 +0900

    3

--- introducing/3_variables/main.go
+++ introducing/3_variables/main.go
@@ -4,7 +4,23 @@ import "fmt"
 
 func main() {
 	var x string
-	x = "Hello, World"
+	x = "first"
 	fmt.Println(x)
+	x = "second"
+	fmt.Println(x)
+	
+	x = "first "
+	fmt.Println(x)
+	x = x + "second"
+	fmt.Println(x)
+	
+	x = "hello"
+	y = "world"
+	fmt.Println(x == y)
+
+	x = "hello"
+	y = "hello"
+	fmt.Println(x == y)
+
 	
 }
