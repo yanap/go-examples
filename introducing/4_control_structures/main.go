7d12cdae50823d1671dd3c49f14446495c3b5eb1
masahiro.yanai <masahiro.yanai@showroom.co.jp>
2020-11-13 14:44:45 +0900

    4 switch

--- introducing/4_control_structures/main.go
+++ introducing/4_control_structures/main.go
@@ -46,4 +46,17 @@ func main() {
 			fmt.Println("divisible by 4")
 		}
 	}
+	if i == 0 {
+		fmt.Println("Zero")
+	} else if i == 1 {
+		fmt.Println("One")
+	} else if i == 2 {
+		fmt.Println("Two")
+	} else if i == 3 {
+		fmt.Println("Three")
+	} else if i == 4 {
+		fmt.Println("Four")
+	} else if i == 5 {
+		fmt.Println("Five")
+	}
 }
