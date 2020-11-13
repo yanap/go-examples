db3356f56cef7bbcb7cf857ab0d53eb040f98c42
masahiro.yanai <masahiro.yanai@showroom.co.jp>
2020-11-13 16:08:25 +0900

    5 f

--- introducing/6_functions/main.go
+++ introducing/6_functions/main.go
@@ -4,10 +4,24 @@ import "fmt"
 
 func main() {
 	xs := []float64{98, 93, 77, 82, 83}
+	fmt.Println(average(xs))
+	
+	someOtherName := []float64{98, 93, 77, 82, 83}
+	fmt.Println(average(someOtherName))
+
+	f()
+}
+
+func average(xs []float64) float64 {
+	//panic("Not Implemented")
 	total := 0.0
 	for _, v := range xs {
 		total += v
 	}
-	fmt.Println(total / float64(len(xs)))
+	return total / float64(len(xs))
 }
 
+var x int = 5
+func f() {
+	fmt.Println(x)
+}
