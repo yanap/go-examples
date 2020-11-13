f30a82aa22f293af9bd752f3ccd021169c2768f2
masahiro.yanai <masahiro.yanai@showroom.co.jp>
2020-11-13 11:03:18 +0900

    2 booleans

--- introducing/2_types/main.go
+++ introducing/2_types/main.go
@@ -4,8 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("1 + 1 =", 1 + 1)
-	
+	// strings
 	fmt.Println(len("Hello World"))
 	fmt.Println("Hello World"[1])
 	fmt.Println("Hello," + "World")
+	// booleans
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
