3189132fd6ced3658923d26bd5c7d4c2845de4ba
masahiro.yanai <masahiro.yanai@showroom.co.jp>
2020-11-13 16:01:09 +0900

    5

--- introducing/5_arrays_slices_and_maps/map.go
+++ introducing/5_arrays_slices_and_maps/map.go
@@ -6,7 +6,8 @@ func main() {
 	// a()
 	// b()
 	// c()
-	d()
+	// d()
+	f()
 }
 
 func a() {
@@ -48,4 +49,69 @@ func d() {
 	
 	name, ok := elements["Un"]
 	fmt.Println(name, ok)
+}
+
+func e() {
+	elements := map[string]string{
+		"H": "Hydrogen",
+		"He": "Helium",
+		"Li": "Lithium",
+		"Be": "Beryllium",
+		"B": "Boron",
+		"C": "Carbon",
+		"N": "Nitrogen",
+		"O": "Oxygen",
+		"F": "Flurrine",
+		"Ne": "Neon",
+	}
+	fmt.Println(elements)
+}
+
+func f() {
+	elements := map[string]map[string]string{
+		"H": map[string]string{
+			"name": "Hydrogen",
+			"state": "gas",
+		},
+		"He": map[string]string{
+			"name": "Helium",
+			"state": "gas",
+		},
+		"Li": map[string]string{
+			"name": "Lithium",
+			"state": "solid",
+		},
+		"Be": map[string]string{
+			"name": "Beryllium",
+			"state": "solid",
+		},
+		"B": map[string]string{
+			"name": "Boron",
+			"state": "solid",
+		},
+		"C": map[string]string{
+			"name": "Carbon",
+			"state": "solid",
+		},
+		"N": map[string]string{
+			"name": "Nitrogen",
+			"state": "gas",
+		},
+		"O": map[string]string{
+			"name": "Oxygen",
+			"state": "gas",
+		},
+		"F": map[string]string{
+			"name": "Flurrine",
+			"state": "gas",
+		},
+		"Ne": map[string]string{
+			"name": "Neon",
+			"state": "gas",
+		},
+	}
+
+		if el, ok := elements["Li"]; ok {
+			fmt.Println(el["name"], el["state"])
+		}
 }
