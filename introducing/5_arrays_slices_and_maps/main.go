c392235829ea5f97357c10b93d5301af22f89a34
masahiro.yanai <masahiro.yanai@showroom.co.jp>
2020-11-13 15:32:56 +0900

    slice

--- introducing/5_arrays_slices_and_maps/main.go
+++ introducing/5_arrays_slices_and_maps/main.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	// array
 	var x [5]int
 	x[4] = 100
 	fmt.Println(x)
+	// slice
+	// var x []float64
+	// x := make([]float64, 5)
+	// x := make([]float64, 5, 10)
+	// arr := [5]float64{1,2,3,4,5}
+	// x := arr[0:5]	
+	
 }
