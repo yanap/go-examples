04147a4af176953e7a2f3127f7c53fee7426aa03
masahiro.yanai <masahiro.yanai@showroom.co.jp>
2020-11-13 15:15:00 +0900

    fix

--- introducing/4_control_structures/main.go
+++ introducing/4_control_structures/main.go
@@ -59,4 +59,13 @@ func main() {
 	} else if i == 5 {
 		fmt.Println("Five")
 	}
+	switch i {
+	case 0: fmt.Println("Zero")
+	case 1: fmt.Println("One")
+	case 2: fmt.Println("Two")
+	case 3: fmt.Println("Three")
+	case 4: fmt.Println("Four")
+	case 5: fmt.Println("Five")
+	default: fmt.Println("Unknown Number")
+	}
 }
