2d595d8d29f1bcb426b3563b82b7abd699f274fb
masahiro.yanai <masahiro.yanai@showroom.co.jp>
2020-11-13 16:21:31 +0900

    closure

---
+++ introducing/6_functions/closure.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 0
+	increment := func() int {
+		x++
+		return x
+	}
+	fmt.Println(increment())
+	fmt.Println(increment())
+	nextEven := makeEvenGenerator()
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+}
+
+func makeEvenGenerator() func() uint {
+	i := uint(0)
+	return func() (ret uint) {
+		ret = i
+		i += 2
+		return
+	}
+}
