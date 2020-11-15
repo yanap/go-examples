5529b011fe589f6f2670e8a7a2a1a1339fb8d519
masahiro.yanai <masahiro.yanai@showroom.co.jp>
2020-11-15 21:56:11 +0900

    8 count

--- introducing/8_packages/main.go
+++ introducing/8_packages/main.go
@@ -9,4 +9,7 @@ func main() {
 	// Contains(s, substr string) bool
 	fmt.Println(strings.Contains("test", "es"))
 	// => true
+	
+	// func Count(s, sep string) int
+	fmt.Println(strings.Count("test", "t"))
 }
