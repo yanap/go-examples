2b731decb94c7724253f6d1f7837340e07a83b93
masahiro.yanai <masahiro.yanai@showroom.co.jp>
2020-11-17 20:44:00 +0900

    8 join

--- introducing/8_packages/main.go
+++ introducing/8_packages/main.go
@@ -9,7 +9,7 @@ func main() {
 	// Contains(s, substr string) bool
 	fmt.Println(strings.Contains("test", "es"))
 	// => true
-	
+
 	// func Count(s, sep string) int
 	fmt.Println(strings.Count("test", "t"))
 	// 2
@@ -17,8 +17,16 @@ func main() {
 	// func HasPrefix(s, prefix string) bool
 	fmt.Println(strings.HasPrefix("test", "te"))
 	// => true
-	
+
 	// func HasSuffix(s, suffix string) bool
 	fmt.Println(strings.HasSuffix("test", "st"))
-	// => true 
+	// => true
+
+	// func Index(s, sep string) int
+	fmt.Println(strings.Index("test", "e"))
+	// => 1
+
+	// func Join(a []string, sep string) string
+	fmt.Println(strings.Join([]string{"a", "b"}, "-"))
+	// "a-b"
 }
