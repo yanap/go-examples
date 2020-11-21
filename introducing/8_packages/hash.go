f9cdf6e1a73634ec060fcd2fb4974aa7ef0c06a6
masahiro.yanai <masahiro.yanai@showroom.co.jp>
2020-11-21 22:28:32 +0900

    8 hash

--- introducing/8_packages/hash.go
+++ introducing/8_packages/hash.go
@@ -3,7 +3,31 @@ package main
 import (
 	"fmt"
 	"hash/crc32"
+	"io"
+	"os"
 )
+
+func getHash(filename string) (uint32, error) {
+	// open the file
+	f, err := os.Open(filename)
+	if err != nil {
+		return 0, err
+	}
+	// remember to always close opened files
+	defer f.Close()
+
+	// create a hasher
+	h := crc32.NewIEEE()
+	// copy the file into the hasher
+	// - copy takes (dst, src) and returns (bytesWritten, error)
+	_, err = io.Copy(h, f)
+	// we don't care about how many bytes were written, but do want to
+	// handle the error
+	if err != nil {
+		return 0, err
+	}
+	return h.Sum32(), nil
+}
 
 func main() {
 	// create a hasher
@@ -13,4 +37,14 @@ func main() {
 	// calcurate the crc32 checksum
 	v := h.Sum32()
 	fmt.Println(v)
+
+	h1, err := getHash("test1.txt")
+	if err != nil {
+		return
+	}
+	h2, err := getHash("test2.txt")
+	if err != nil {
+		return
+	}
+	fmt.Println(h1, h2, h1 == h2)
 }
