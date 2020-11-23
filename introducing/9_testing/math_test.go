3f6778d37b0d20f79e001ed43dd8e0659265ce7d
masahiro.yanai <masahiro.yanai@showroom.co.jp>
2020-11-23 23:23:04 +0900

    9

--- introducing/9_testing/math_test.go
+++ introducing/9_testing/math_test.go
@@ -1,8 +1,27 @@
 package math
 
-func TestAverage(t *testing) {
-	v := Average([]float64{1, 2})
-	if v != 1.5 {
-		t.Error("Expected 1.5, got", v)
+import "testing"
+
+type testpair struct {
+	values  []float64
+	average float64
+}
+
+var tests = []testpair{
+	{[]float64{1, 2}, 1.5},
+	{[]float64{1, 1, 1, 1, 1, 1}, 1},
+	{[]float64{-1, 1}, 0},
+}
+
+func TestAverage(t *testing.T) {
+	for _, pair := range tests {
+		v := Average(pair.values)
+		if v != pair.average {
+			t.Error(
+				"For", pair.values,
+				"Expected 1.5, got", pair.average,
+				"got", v,
+			)
+		}
 	}
 }
