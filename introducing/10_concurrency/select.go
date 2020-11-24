0b72101f600b69612677cfa68035f1c0ff68b4bb
masahiro.yanai <masahiro.yanai@showroom.co.jp>
2020-11-24 09:58:47 +0900

    select

--- introducing/10_concurrency/select.go
+++ introducing/10_concurrency/select.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	// c := make(chan int, 1)
 	c1 := make(chan string)
 	c2 := make(chan string)
 
@@ -27,9 +28,13 @@ func main() {
 		for {
 			select {
 			case msg1 := <-c1:
-				fmt.Println((msg1))
+				fmt.Println(msg1)
 			case msg2 := <-c2:
 				fmt.Println(msg2)
+			case <-time.After(time.Second):
+				fmt.Println("timeout")
+			default:
+				fmt.Println("nothing ready")
 			}
 		}
 	}()
