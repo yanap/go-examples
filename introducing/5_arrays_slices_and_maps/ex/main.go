ee50adc26f9be975c51f08c5cfcb13d97b96f4be
masahiro.yanai <masahiro.yanai@showroom.co.jp>
2020-11-27 19:26:00 +0900

    5 exercise

---
+++ introducing/5_arrays_slices_and_maps/ex/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var min int
+	x := []int{
+		48, 96, 86, 68,
+		57, 82, 63, 70,
+		37, 34, 83, 27,
+		19, 97, 9, 17,
+	}
+	for i, v := range x {
+		if i == 0 || v < min {
+			min = v
+		}
+	}
+	fmt.Println(min)
+}
